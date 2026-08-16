09ec47b6e0291dfa4528891d23c51d5e8a4b6e6b
Kishu-Kumar-20 <kkeshriwal@gmail.com>
2026-08-16 13:17:48 +0530

    Time: 4 ms (10.51%), Space: 71.8 MB (15.17%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -5,7 +5,7 @@ public:
         for(int i = 0; i<nums.size(); i++){
             _sum += nums[i];
             _max = max(_sum, _max);
-            if(_sum < 0) _sum = 0;
+            _sum = max(_sum, 0LL);
         }
         return _max;
     }
