a1fdcb125b8a66c028a708e626f89f8d2f87e864
Kishu-Kumar-20 <kkeshriwal@gmail.com>
2026-08-30 19:37:08 +0530

    Time: 0 ms (100%), Space: 11.7 MB (14.69%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,25 +1,19 @@
 class Solution {
 public:
-    void allperm(vector<int> nums, vector<vector<int>> &ans, vector<int> &freq, vector<int> &add){
-        if(add.size() == nums.size()){
-            ans.push_back(add);
+    void opt_perm(int index, vector<int> nums, vector<vector<int>> &ans){
+        if(index == nums.size()){
+            ans.push_back(nums);
             return;
         }
-        for(int i = 0; i<nums.size(); i++){
-            if(!freq[i]){
-                add.push_back(nums[i]);
-                freq[i] = 1;
-                allperm(nums, ans, freq, add);
-                freq[i] = 0;
-                add.pop_back();
-            }
+        for(int i = index; i < nums.size(); i++){
+            swap(nums[index], nums[i]);
+            opt_perm(index+1, nums, ans);
+            // swap(nums[index], nums[i]);
         }
     }
     vector<vector<int>> permute(vector<int>& nums) {
         vector<vector<int>> ans;
-        vector<int> freq (nums.size(), 0);
-        vector<int> add;
-        allperm(nums, ans, freq, add);
+        opt_perm(0, nums, ans);
         return ans;
     }
 };
