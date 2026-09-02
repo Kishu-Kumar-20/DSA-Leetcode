2c3722dec62f5f0c4f901815eda351c8fb5b5656
Kishu-Kumar-20 <kkeshriwal@gmail.com>
2026-09-02 20:07:22 +0530

    Time: 0 ms (100%), Space: 30.4 MB (36.43%) - LeetHub

--- 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.cpp
+++ 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.cpp
@@ -1,33 +1,36 @@
 class Solution {
 public:
     bool uniformArray(vector<int>& nums1) {
-        int even = INT_MIN, odd = INT_MAX;
-        vector<int> v;
-        int f_o = 1, f_e = 1;
-        for(int i = 0; i < nums1.size(); i++){
-            if(nums1[i]%2 == 0) even = nums1[i];
-            else odd = nums1[i];
-        }
-        for(int i = 0; i < nums1.size(); i++){
-            if(nums1[i]%2 == 0){
-                continue;
-            }else{
-                if(odd == INT_MAX){
-                    f_e = 0;
-                    break;
-                }
-            }
-        }
-        for(int i = 0; i < nums1.size(); i++){
-            if(nums1[i]%2 != 0){
-                continue;
-            }else{
-                if(even == INT_MIN){
-                    f_o = 0;
-                }
-            }
-        }
-        if(f_o || f_e) return true;
-        else return false;
+        // int even = INT_MIN, odd = INT_MAX;
+        // vector<int> v;
+        // int f_o = 1, f_e = 1;
+        // for(int i = 0; i < nums1.size(); i++){
+        //     if(nums1[i]%2 == 0) even = nums1[i];
+        //     else odd = nums1[i];
+        // }
+        // for(int i = 0; i < nums1.size(); i++){
+        //     if(nums1[i]%2 == 0){
+        //         continue;
+        //     }else{
+        //         if(odd == INT_MAX){
+        //             f_e = 0;
+        //             break;
+        //         }
+        //     }
+        // }
+        // for(int i = 0; i < nums1.size(); i++){
+        //     if(nums1[i]%2 != 0){
+        //         continue;
+        //     }else{
+        //         if(even == INT_MIN){
+        //             f_o = 0;
+        //         }
+        //     }
+        // }
+        // if(f_o || f_e) return true;
+        // else return false;
+
+        // ALL THIS BULLSHIT FOR NOTHING!!
+        return true;
     }
 };
