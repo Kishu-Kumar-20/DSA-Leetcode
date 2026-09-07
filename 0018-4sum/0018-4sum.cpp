b4501ed5e945f2e28f4d4b9898d445ca01fbc6ec
Kishu-Kumar-20 <kkeshriwal@gmail.com>
2026-09-07 19:30:40 +0530

    Time: 25 ms (29.14%), Space: 17.6 MB (25.35%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -1,25 +1,35 @@
 class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
-        set<vector<int>> st;
+        sort(nums.begin(), nums.end());
         int n = nums.size();
-        for(int i = 0; i< n; i++){
-            for(int j = i+1; j < n; j++){
-                unordered_set<long long> hash;
-                for(int k = j+1; k< n;k++){
-                    long long sum = nums[i]+nums[j];
+        vector<vector<int>> ans;
+        for(int i = 0; i < n ; i++){
+            if(i>0 && nums[i] == nums[i-1]) continue;
+            for(int j = i+1; j < n ; j++){
+                if(j>i+1 && nums[j] == nums[j-1]) continue;
+                int k = j+1;
+                int l = n-1;
+                while(k<l){
+                    long long  sum = nums[i] ;
+                    sum += nums[j];
                     sum += nums[k];
-                    long long forth = target - sum;
-                    if(hash.find(forth) != hash.end()){
-                        vector<int> temp = {nums[i], nums[j], nums[k], (int) (forth)};
-                        sort(temp.begin(), temp.end());
-                        st.insert(temp);
-                    }
-                    hash.insert(nums[k]);
+                    sum += nums[l];
+                    if(sum<target){
+                        k++;
+                    }else if (sum > target){
+                        l--;
+                    }else{
+                        vector<int> temp = {nums[i], nums[j], nums[k], nums[l]};
+                        ans.push_back(temp);
+                        k++;
+                        l--;
+                        while(nums[k] == nums[k-1] && k <l) k++;
+                        while(nums[l] == nums[l+1] && k <l) l--;
+                    } 
                 }
             }
         }
-        vector<vector<int>> ans(st.begin(), st.end());
         return ans;
     }
 };
