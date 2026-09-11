ef48ff5b16b70abce832b4dbb185241c2172c338
Kishu-Kumar-20 <kkeshriwal@gmail.com>
2026-09-11 20:39:04 +0530

    Time: 516 ms (47.41%), Space: 243.4 MB (45.87%) - LeetHub

--- 0493-reverse-pairs/0493-reverse-pairs.cpp
+++ 0493-reverse-pairs/0493-reverse-pairs.cpp
@@ -1,37 +1,31 @@
 class Solution {
 public:
-    int merge(vector<int> &arr, int low, int mid, int high){
+    void merge(vector<int> &arr, int low, int mid, int high){
         int l = low, r = mid+1;
-        int i = 0, c = 0;
         vector<int> temp;
-        long long ele = arr[mid+1];
-        int x = mid+1;
-        for(int i = low; i < mid+1; i++){
-            while((x <= high) && ((long long)arr[i] > 2 * (long long) arr[x])){
-                c+= (mid-i+1);
-                x++;
-                // ele = arr[x];
-            }
-        }
         while(l<=mid && r<=high){
             if(arr[l] > arr[r]){
                 temp.push_back(arr[r++]);
-                i++;
             }else {
                 temp.push_back(arr[l++]);
-                i++;
             }
         }
         while(l<=mid){
             temp.push_back(arr[l++]);
-            i++;
         }
         while(r <= high){
             temp.push_back(arr[r++]);
-            i++;
         }
         for(int i = low; i <= high; i++){
             arr[i] = temp[i-low];
+        }
+    }
+    int countreverse(vector<int> &arr, int low, int mid, int high){
+        int c = 0;
+        int right = mid+1;
+        for(int i = low; i < mid+1; i++){
+            while((right <= high) && ((long long)arr[i] > 2 * (long long) arr[right])) right++;
+            c += (right - (mid+1));
         }
         return c;
     }
@@ -41,7 +35,8 @@ public:
         int mid = (low+high)/2;
         count += findinversions(arr, low, mid);
         count += findinversions(arr, mid+1, high);
-        count += merge(arr, low, mid, high);
+        count += countreverse(arr, low, mid, high);
+        merge(arr, low, mid, high);
         return count;
     }
     int reversePairs(vector<int>& nums) {
