10c44ee4f5abb2ea47b5d1efb5efde80233c5a9d
Kishu-Kumar-20 <kkeshriwal@gmail.com>
2026-09-15 22:31:13 +0530

    Time: 28 ms (5.04%), Space: 23 MB (58.76%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -1,9 +1,12 @@
 class Solution {
 public:
-    long long caltime(vector<int> &arr, int r){
+    long long caltime(vector<int> &arr, int r, int h){
         long long time = 0;
         for(int i = 0; i < arr.size(); i++){
-            time += ceil((double)arr[i]/r); 
+            time += (arr[i] + r - 1) / r; 
+            
+            if (time > h)
+                return time;
         }
         return time;
     }
@@ -11,7 +14,7 @@ public:
         int low = 1, high = *max_element(piles.begin(), piles.end());
         while(low <= high){
             int mid = low + (high-low)/2;
-            long long time = caltime(piles, mid);
+            long long time = caltime(piles, mid, h);
             if(time <= h){
                 high = mid-1;
             }else{
